fcdf64c3a9b2c5e5083f6d577e4d0a422fceb414
datalogics-dliang <dliang@datalogics.com>
2025-02-20 12:59:07 -0600

    Update nav

--- app.config.ts
+++ app.config.ts
@@ -27,28 +27,10 @@ export default defineAppConfig({
       },
       darkModeToggle: true,
       nav: [{
-        title: 'Credits',
-        links: [{
-          title: 'shadcn-ui',
-          to: 'https://ui.shadcn.com/',
-          description: 'For the beautiful component design & docs design',
-          target: '_blank',
-        }, {
-          title: 'shadcn-vue',
-          to: 'https://www.shadcn-vue.com/',
-          description: 'For the vue port of shadcn-ui & some docs component source',
-          target: '_blank',
-        }, {
-          title: 'Docus',
-          to: 'https://docus.dev/',
-          description: 'For inspiration & some docs component source',
-          target: '_blank',
-        }, {
-          title: 'Nuxt Content',
-          to: 'https://content.nuxt.com/',
-          description: 'Content made easy for Vue Developers',
-          target: '_blank',
-        }],
+        title: 'pdfRest',
+        to: 'https://pdfrest.com/',
+        target: '_blank',
+        showLinkIcon: true,
       }],
       links: [{
         icon: 'lucide:github',
